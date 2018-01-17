f957491dfc2eefc7b9f595927e486e62f9717956
Eu <pedrohbh@users.noreply.github.com>
2018-01-16 22:12:41 -0200

    Modificações na Ramificação

--- Hello.c
+++ Hello.c
@@ -3,6 +3,12 @@
 
 int main()
 {
+   int a;
+   int b;
    printf("Olá mundo!\n");
+   printf("Digite dois números: ");
+   scanf("%d %d", &a, &b );
+   
+   printf("A soma dos dois números é: %d\n", (a + b));
    return 0;
 }
